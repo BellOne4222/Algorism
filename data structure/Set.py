ab052de630baf678a48d102f4b34cd64fadcb46f
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-03-29 18:06:01 +0900

    Update Set.py

--- data structure/Set.py
+++ data structure/Set.py
@@ -1,12 +1,12 @@
 # 집합
 
-s = set()
+s = set() # 집합
 s.add(456)
 s.add(12)
 s.add(456)
 s.add(7890)
 s.add(7890)
-s.add(456)
+s.add(456) # 중복 값은 안되므로 456, 12 ,7890 밖에 안들어가서 길이는 3이 나온다.
 
 print("size : " , len(s))
 for i in s:
